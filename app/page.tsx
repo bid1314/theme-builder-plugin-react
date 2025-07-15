a7976659c86e8f04694b45c96af407baa792f005
Brieana Davis <bid15@txstate.edu>
2025-07-15 22:05:32 +0000

    Add app/page.tsx 1752617130398

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import UIBuilder from "@/components/ui-builder"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <UIBuilder />
+    </main>
+  )
+}
